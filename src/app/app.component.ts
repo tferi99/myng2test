161334b80bcb224a8026cb181ebee997937664a5
tferi99 <toth_ferenc@hotmail.com>
2017-02-23 21:36:53 +0100

    - displaying data - user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works v4!';
+  value1 : string = "Value 1";
+  value2 : string = "Value 2";
+  value3 : string = "Value 3";
+
+  num1 : number = 1;
+  keyInputValue: string;
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  onClickme() {
+    this.num1++;
+  }
+
+  onKeyUp(event : any) {
+    this.keyInputValue = event.target.value;
+  }
 }
